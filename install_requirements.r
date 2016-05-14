0b57b79cb298e595d4bfe31eb9dfffc51a53b59f
Fabian-Robert Stöter <mail@faroit.com>
2016-05-14 09:20:18 +0200

    first version

---
+++ install_requirements.r
@@ -0,0 +1,11 @@
+install.packages('rmarkdown', dependencies = TRUE);
+install.packages('ggplot2', dependencies = TRUE);
+install.packages('Hmisc', dependencies = TRUE);
+install.packages('icd', dependencies = TRUE);
+install.packages('xts', dependencies = TRUE);
+install.packages('tscount', dependencies = TRUE);
+install.packages('Hmisc', dependencies = TRUE);
+library(devtools)
+install_github('sinhrks/ggfortify')
+cat("\014")
+setwd("~/Material")
