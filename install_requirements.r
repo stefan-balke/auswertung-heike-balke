c4d4aeab713c3b842f0d69ac7079b65021e7c3b9
Stefan Balke <stefan.balke@audiolabs-erlangen.de>
2016-05-28 18:20:29 +0200

    devtools were missing

--- install_requirements.r
+++ install_requirements.r
@@ -5,7 +5,8 @@ install.packages('icd', dependencies = TRUE);
 install.packages('xts', dependencies = TRUE);
 install.packages('tscount', dependencies = TRUE);
 install.packages('Hmisc', dependencies = TRUE);
+install.packages('devtools', dependencies = TRUE);
 library(devtools)
 install_github('sinhrks/ggfortify')
+install.packages('forecast', dependencies = TRUE);
 cat("\014")
-setwd("~/Material")
